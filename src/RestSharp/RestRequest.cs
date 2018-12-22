e134763f3996eaf6385c06ec2978b11145456283
Sergey <xxsmotur@gmail.com>
2018-12-23 02:01:58 +0500

    RestRequest: Implementation of AddParameters

--- src/RestSharp/RestRequest.cs
+++ src/RestSharp/RestRequest.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace RestSharp {
 
     /// <summary>
@@ -5,8 +7,17 @@ namespace RestSharp {
     /// </summary>
     public class RestRequest: IRestRequest {
         private string Url { get; private set; }
+        private Dictionary<string, string> parameters;
         public RestRequest(url string) {
             Url = url;
+            parameters = new Dictionary<string, string>();
+        }
+
+        /// <summary>
+        /// Adding of the Parameters in the case of POST request
+        /// </summary>
+        public void AddHeader(string name, string value) {
+            parameters.Add(name, value);
         }
     } 
 }
