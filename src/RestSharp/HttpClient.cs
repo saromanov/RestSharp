36305ca2a089d8f10c8584a4b39e0eced3ba3bb3
Sergey <xxsmotur@gmail.com>
2018-12-23 01:48:16 +0500

    Draft for execution on HttpClient

--- src/RestSharp/HttpClient.cs
+++ src/RestSharp/HttpClient.cs
@@ -4,5 +4,20 @@ namespace RestSharp {
         public HttpClient(){
             
         }
+
+        /// <summary>
+        /// Do provides executing of the request
+        //  with defined type 
+        /// </summary>
+        public T Do<T>(IRestRequest request) {
+            var result = request.Execute();
+        }
+
+        /// <summary>
+        /// Do provides executing of the request
+        /// </summary>
+        public Do(IRestRequest request) {
+            var result = request.Execute();
+        }
     }
 }
