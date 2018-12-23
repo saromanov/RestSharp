1862c054d56cdd263628a34b8cc8d11e5d67b5b6
Sergey <xxsmotur@gmail.com>
2018-12-23 15:47:30 +0500

    Add method for apply headers on request

--- src/RestSharp/RestRequest.cs
+++ src/RestSharp/RestRequest.cs
@@ -8,16 +8,26 @@ namespace RestSharp {
     public class RestRequest: IRestRequest {
         private string Url { get; private set; }
         private Dictionary<string, string> parameters;
+        private Dictionary<string, string> headers;
         public RestRequest(url string) {
             Url = url;
             parameters = new Dictionary<string, string>();
+            headers = new Dictionary<string, string>();
         }
 
         /// <summary>
         /// Adding of the Parameters in the case of POST request
         /// </summary>
+        public void AddParameter(string name, string value) {
+            parameters.Add(name, value);
+        }
+
+        /// <summary>
+        /// Adding of the Header on request
+        /// </summary>
         public void AddHeader(string name, string value) {
             parameters.Add(name, value);
         }
+
     } 
 }
