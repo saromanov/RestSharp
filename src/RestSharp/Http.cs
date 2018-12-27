b9f2538ca89a7a3734639b6fd737a2e8226f47e8
Sergey <xxsmotur@gmail.com>
2018-12-27 23:05:08 +0500

    Add method for create WebRequest

--- src/RestSharp/Http.cs
+++ src/RestSharp/Http.cs
@@ -1,12 +1,17 @@
 using System;
 using System.Net;
-
+using System.Collections;
 namespace RestSharp {
 
     public class Http: IHttp {
 
+        private List<string> Cookies {get; private set; }
+        protected bool HasCookies => Cookies.Any();
+
         public Http() {
-            
+
         }
+
+        private CreateWebRequest(Uri uri) => WebRequest.Create(uri);
     }
 }
